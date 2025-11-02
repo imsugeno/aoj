426dd6163b593910bff0f82513b8a292e2a46f9f
imsugeno <g.tokyo.kazusa@gmail.com>
2025-11-02 23:11:04 +0900

    selection sort

--- aoj/alds2/b.py
+++ aoj/alds2/b.py
@@ -2,16 +2,26 @@ def main():
     n = int(input())
     target = list(map(int, input().split()))
 
-    count = 0
-    for i in range(n):
-        for j in range(n-1,i,-1):
-            if target[j-1] > target[j]:
-                tmp = target[j]
-                target[j] = target[j-1]
-                target[j-1] = tmp
-                count += 1
+    target, count = selection_sort(n, target)
+
     print(*target)
     print(count)
+
+def selection_sort(n, target):
+    count = 0
+
+    for i in range(n):
+        min_index = i
+        for j in range(i, n):
+            if target[j] < target[min_index]:
+                min_index = j
+        if min_index != i:
+            tmp = target[i]
+            target[i] = target[min_index]
+            target[min_index] = tmp
+            count += 1
+
+    return target, count
 
 if __name__ == "__main__":
     main()
