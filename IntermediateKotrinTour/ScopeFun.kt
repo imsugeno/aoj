d640ca050e595ea5790aeed7f12eace41fdf1e07
imsugeno <g.tokyo.kazusa@gmail.com>
2025-11-12 23:47:04 +0900

    muzu

--- IntermediateKotrinTour/ScopeFun.kt
+++ IntermediateKotrinTour/ScopeFun.kt
@@ -24,18 +24,71 @@ val client: Client = Client().apply {
     token = "asdf"
 }
 
+data class ProductInfo(val priceInDollars: Double?)
+
+class Product {
+    fun getProductInfo(): ProductInfo  {
+        return ProductInfo(100.0)
+    }
+}
+
+// Rewrite this function
+fun Product.getPriceInEuros() = getProductInfo()?.priceInDollars?.let { convertToEuros(it) }
+
+fun convertToEuros(dollars: Double): Double {
+    return dollars * 0.85
+}
+
+data class User(val id: Int, var email: String)
+
+fun updateEmail(user: User, newEmail: String): User = user.apply { 
+    email = newEmail
+ }.also { 
+    println("Updating email for user with ID: ${it.id}")
+}
+
 fun main() {
     // val address: String? = getNextAddress()
     // val confirm = address?.let {
     //     sendNotification(it)
     // }
-    val result: String = client.run {
-        connect()
-        authenticate()
-        getData()
+    // val result: String = client.run {
+    //     connect()
+    //     authenticate()
+    //     getData()
+    // }
+
+    // println(result)
+
+    // val meadals: List<String> = listOf("Gold", "Silver", "Bronze")
+    // val reversedLongUppercaseMedals: List<String> = 
+    //     meadals
+    //         .map { it.uppercase() }
+    //         .also { println(it) }
+    //         .filter { it.length > 4 }
+    //         .also { println(it) }
+    //         .reversed()
+    //     println(reversedLongUppercaseMedals)
+
+    /*
+    practice 1
+    val product = Product()
+    val priceInEuros = product.getPriceInEuros()
+
+    if (priceInEuros != null) {
+        println("Price in Euros: €$priceInEuros")
+        // Price in Euros: €85.0
+    } else {
+        println("Price information is not available.")
     }
+    */
 
-    println(result)
+    // practice 2
+    val user = User(1, "old_email@example.com")
+    val updatedUser = updateEmail(user, "new_email@example.com")
+    // Updating email for user with ID: 1
 
-    // client.getData()
+    println("Updated User: $updatedUser")
+    // Updated User: User(id=1, email=new_email@example.com)
 }
+
